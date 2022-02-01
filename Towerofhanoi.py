17b779313bae42349c54501d6917216037a23e7d
Your Name <you@example.com>
2022-02-01 12:14:39 +0545

    multiple disk added

--- Towerofhanoi.py
+++ Towerofhanoi.py
@@ -1,10 +1,28 @@
 import pygame
+n=5
+moves=0
+width=700
+height=500
+getx=[]
+pygame.init()
+color=['red','blue','white','green','yellow','white']
+screen=pygame.display.set_mode((width,height))
+rod_image='rod.png'
+rod=pygame.image.load(rod_image)
+rods=pygame.transform.scale(rod, (100,200))
+display='Welcome to the tower of hanoi'
+font = pygame.font.Font('freesansbold.ttf', 18)
+text = font.render(display, True,'green','blue')
 class stack:
     def __init__(self):
         self.top=-1
-        self.number=[0,0,0]
+        self.number=[]
+        temp=n
+        while temp!=0:
+            temp-=1
+            self.number.append(0)
     def push(self,number):
-        if self.top<3:
+        if self.top<n:
             self.top+=1
             self.number[self.top]=number
     def pop (self):
@@ -15,34 +33,30 @@ class stack:
             return temp
     def see(self):
         saw=[]
-        temp=2
-        while temp>=0:
+        temp=0
+        while temp<n:
             saw.append(self.number[temp])
-            temp-=1
+            temp+=1
         return saw
     def seetop(self):
         if self.top==-1:
-            return 4
+            return n+1
         else:
             return self.number[self.top]
-width=700
-height=500
-getx=[]
-pygame.init()
-color=['red','blue','white','green']
-screen=pygame.display.set_mode((width,height))
-rod_image='rod.png'
-rod=pygame.image.load(rod_image)
-rods=pygame.transform.scale(rod, (100,200))
-display='Welcome to the tower of hanoi'
-font = pygame.font.Font('freesansbold.ttf', 18)
-text = font.render(display, True,'green','blue')
+    def combo(self,num,pos):
+        
+        if num !=0:
+            state=pygame.draw.rect(screen, color[num-1], pygame.Rect(posx[num-1]+200*i,posy[pos], size+20*num, 15))
+            return state
+def load(number):
+    while number!=0:
+        d[0].push(number)
+        number-=1
 d=[stack(),stack(),stack()]
-d[0].push(3)
-d[0].push(2)
-d[0].push(1)
-posx=[90,98,106]
-posy=[480,460,440]
+load(n)
+initial=d[0].see()
+posx=[122,114,106,98,90]
+posy=[480,460,440,420,400]
 size=80
 while 1:
     screen.fill(0)
@@ -51,31 +65,19 @@ while 1:
     screen.blit(rods,[500,300])
     textRect = text.get_rect()
     textRect.center = (350, 100)
+    text = font.render(display, True,'green','blue')
     screen.blit(text, textRect)
     for i in range(3):
-        if d[i].see()==[1,2,3]:
-            pygame.draw.rect(screen, color[0], pygame.Rect(posx[0]+200*i,posy[0], size+40, 15))
-            pygame.draw.rect(screen, color[1], pygame.Rect(posx[1]+200*i,posy[1], size+20, 15))
-            pygame.draw.rect(screen, color[2], pygame.Rect(posx[2]+200*i,posy[2], size, 15))
-        elif d[i].see()==[0,1,2]:
-            pygame.draw.rect(screen, color[1], pygame.Rect(posx[1]+200*i,posy[0], size+20, 15))
-            pygame.draw.rect(screen, color[2], pygame.Rect(posx[2]+200*i,posy[1], size, 15))
-        elif d[i].see()==[0,1,3]:
-            pygame.draw.rect(screen, color[0], pygame.Rect(posx[0]+200*i,posy[0], size+40, 15))
-            pygame.draw.rect(screen, color[2], pygame.Rect(posx[2]+200*i,posy[1], size, 15))
-        elif d[i].see()==[0,2,3]:
-            pygame.draw.rect(screen, color[0], pygame.Rect(posx[0]+200*i,posy[0], size+40, 15))
-            pygame.draw.rect(screen, color[1], pygame.Rect(posx[1]+200*i,posy[1], size+20, 15))
-        elif d[i].see()==[0,0,1]:
-            pygame.draw.rect(screen, color[2], pygame.Rect(posx[2]+200*i,posy[0], size, 15))
-        elif d[i].see()==[0,0,2]:
-            pygame.draw.rect(screen, color[1], pygame.Rect(posx[1]+200*i,posy[0], size+20, 15))
-        elif d[i].see()==[0,0,3]:
-            pygame.draw.rect(screen, color[0], pygame.Rect(posx[0]+200*i,posy[0], size+40, 15))
+        pos=0
+        for j in d[i].see():
+            if j!=0:
+                d[i].combo(j,pos)
+                pos+=1
     pygame.display.flip()
+
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
-            pygame.quit() 
+            pygame.quit()
             exit(0)
         if event.type==pygame.MOUSEBUTTONDOWN:
             x,y=pygame.mouse.get_pos()
@@ -86,29 +88,40 @@ while 1:
                     if getx[k-1]>200 and getx[k-1]<400:
                         if d[0].seetop()>d[1].seetop():
                             d[0].push(d[1].pop())
+                            moves+=1
                     elif getx[k-1]>400 and getx[k-1]<600:
                         if d[0].seetop()>d[2].seetop():
                             d[0].push(d[2].pop())
+                            moves+=1
                     else:
                         getx.clear()
                 elif x>200 and x<400:
                     if getx[k-1]<200 and getx[k-1]>0:
                         if d[1].seetop()>d[0].seetop():
                             d[1].push(d[0].pop())
+                            moves+=1
                     elif getx[k-1]>400 and getx[k-1]<600:
                         if d[1].seetop()>d[2].seetop():
                             d[1].push(d[2].pop())
+                            moves+=1
                     else:
                         getx.clear()
                 elif x>400 and x<600:
                     if getx[k-1]>200 and getx[k-1]<400:
                         if d[2].seetop()>d[1].seetop():
                             d[2].push(d[1].pop())
+                            moves+=1
                     elif getx[k-1]>0 and getx[k-1]<200:
                         if d[2].seetop()>d[0].seetop():
                             d[2].push(d[0].pop())
+                            moves+=1
                     else:
                         getx.clear()
-    if d[1].see()==[1,2,3] or d[2].see()==[1,2,3]:
-        text = font.render("Congo You completed the game", True,'green','blue')
-        screen.blit(text, textRect)
+        
+    if d[1].see()==initial or d[2].see()==initial:
+        if moves==2**n-1:
+            display="Game completed with least moves "+str(moves)
+        else:
+            display="Game completed with "+str(moves)+" moves"
+    else:
+        display='moves = '+str(moves)+' Least Moves Possible = '+str(2**n-1)
